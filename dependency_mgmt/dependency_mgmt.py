05624520026c9e9354c8e6aef01aac829a134cb0
TomiJolkkonen <tomi.jolkkonen@skillio.ai>
2025-03-14 09:53:31 +0200

    esityksia

--- dependency_mgmt/dependency_mgmt.py
+++ dependency_mgmt/dependency_mgmt.py
@@ -1,14 +1,14 @@
 import requests
 
-def get_joke():
+def etsi_vitsi():
     url = "https://official-joke-api.appspot.com/random_joke"
     try:
-        response = requests.get(url)
-        response.raise_for_status()
-        joke = response.json()
-        print(f"{joke['setup']} - {joke['punchline']}")
+        responssi = requests.get(url)
+        responssi.raise_for_status()
+        vitsi = responssi.json()
+        print(f"{vitsi['setup']} - {vitsi['punchline']}")
     except requests.exceptions.RequestException as e:
-        print(f"Error fetching joke: {e}")
+        print(f"Tuli virhe: {e}")
 
 if __name__ == "__main__":
-    get_joke()
+    etsi_vitsi()
